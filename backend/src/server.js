99d5116b7d77d398f9b8d2e8f4cf6b57fdb4b1ae
Ashu-99-dev <ashutiwari9901@gmail.com>
2025-07-06 14:37:57 +0530

    adding chat route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import  "dotenv/config";
-import authRoutes from "./routes/auth.route.js";
-import { connectDB } from "./lib/db.js";
 import  cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
+import { connectDB } from "./lib/db.js";
 
 
 const app = express();
@@ -13,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users",userRoutes);
+app.use("/api/chats",chatRoutes);
 
 
 app.listen(PORT, () => {
